837ff9792fc6f003bfec9f47161ff8691b064f9e
plachert <piotr@mxlabs.ai>
2022-06-20 07:34:51 +0000

    Add main

--- train.py
+++ train.py
@@ -3,12 +3,17 @@ from models.basic_autoencoder import AutoEncoderSystem, Encoder, Decoder
 from pytorch_lightning import Trainer
 
 
-encoder = Encoder()
-decoder = Decoder()
-system = AutoEncoderSystem(encoder, decoder)
-datamodule = MNISTDataModule()
-trainer = Trainer(max_epochs=1)
-trainer.fit(system, datamodule)
+def main():
+    encoder = Encoder()
+    decoder = Decoder()
+    system = AutoEncoderSystem(encoder, decoder)
+    datamodule = MNISTDataModule()
+    trainer = Trainer(max_epochs=1)
+    trainer.fit(system, datamodule)
+
+
+if __name__ == "__main__":
+    main()
 
 
 
